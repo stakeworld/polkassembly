4aea6f0d55917ed96a7f2c3ec5db94c7e2713e45
Mridul Kumar <kumar.mridul000@gmail.com>
2022-03-25 07:51:34 +0530

    Removes obsolete todo

--- front-end/src/screens/UserProfile/index.tsx
+++ front-end/src/screens/UserProfile/index.tsx
@@ -2,8 +2,6 @@
 // This software may be modified and distributed under the terms
 // of the Apache-2.0 license. See the LICENSE file for details.
 
-// TODO: Rename file and directory.
-
 import styled from '@xstyled/styled-components';
 import React from 'react';
 import { Button, Divider, Grid, Icon, Label } from 'semantic-ui-react';
