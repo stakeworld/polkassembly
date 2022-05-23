ee1df1e75881e91aff9c2c2644bf208a3cd37209
rajdeep7Singh <sdrajdeep96@gmail.com>
2022-05-23 14:33:14 +0530

    query fix

--- front-end/src/components/queries.ts
+++ front-end/src/components/queries.ts
@@ -45,7 +45,6 @@ export const DELETE_CALENDER_EVENT = gql`
 
 export const ADD_CALENDER_EVENT=gql`
     mutation AddCalenderEvent ($title: String!, $start_time: timestamptz!, $content: String, $end_time: timestamptz!, $url: String, $module: String, $network: String!) {
-        __typename
         insert_calender_events(objects: {title: $title, start_time: $start_time, end_time: $end_time, content: $content, url: $url, module: $module, network: $network}) {
             affected_rows
         }
@@ -54,7 +53,6 @@ export const ADD_CALENDER_EVENT=gql`
 
 export const ADD_BIO_IMAGE=gql`
     mutation addProfile ($image: String!, $bio: String!, $user_id: Int!, $title: String, $badges: String) {
-        __typename
         addProfile(image: $image, bio: $bio, user_id: $user_id, title: $title, badges: $badges) {
             message
         }
