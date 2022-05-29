736966af352aa941029fda111956cce9c4f70039
Nikhil Ranjan <niklabh811@gmail.com>
2022-05-29 21:23:34 +0200

    Revert "On-chain info Arguments table and JSON view"

--- front-end/src/components/Post/PostGovernanceInfo/PostProposalInfo.tsx
+++ front-end/src/components/Post/PostGovernanceInfo/PostProposalInfo.tsx
@@ -11,7 +11,6 @@ import AddressComponent from '../../../ui-components/Address';
 import OnchainInfoWrapper from '../../../ui-components/OnchainInfoWrapper';
 import getNetwork from '../../../util/getNetwork';
 import ExternalLinks from '../../ExternalLinks';
-import ArgumentsTableJSONView from './ArgumentsTableJSONView';
 
 interface Props{
 	onchainLink: OnchainLinkProposalFragment
@@ -50,9 +49,20 @@ const PostProposalInfo = ({ onchainLink }: Props) => {
 						<h6>Method</h6>
 						{method}
 					</Grid.Column>
-					<Grid.Column mobile={16} tablet={16} computer={16}>
+					<Grid.Column mobile={16} tablet={8} computer={8}>
 						{preimageArguments && preimageArguments.length
-							? <ArgumentsTableJSONView postArguments={preimageArguments} showAccountArguments={true}  />
+							? <>
+								<h6>Arguments</h6>
+								{preimageArguments.map((element, index) => {
+									const isAccountArgument = element.name === 'account';
+									return <div className={isAccountArgument ? '' : 'methodArguments'} key={index}>
+										{isAccountArgument
+											? <AddressComponent address={element.value} key={index}/>
+											: <span key={index}>{element.name}: {element.value}</span>
+										}
+									</div>;
+								})}
+							</>
 							: null}
 					</Grid.Column>
 				</Grid.Row>}
