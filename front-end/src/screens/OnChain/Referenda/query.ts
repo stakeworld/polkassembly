0e3bbe2eee8e64a12394224a07b70d3e36b0ce0b
rajdeep7Singh <sdrajdeep96@gmail.com>
2022-09-24 17:56:32 +0530

    query changed

--- front-end/src/screens/OnChain/Referenda/query.ts
+++ front-end/src/screens/OnChain/Referenda/query.ts
@@ -75,17 +75,45 @@ export const QUERY_COUNT_REFERENDA = gql`
     }
 `;
 
-export const GET_VOTES_0F_VOTER = gql`
-    query GetVotesOfVoter($voter: String!) {
-        referendumVotes(where: {voter: $voter}) {
+export const GET_REFERENDA_WITH_VOTES_OF_USER = gql`
+    query GetLatestReferendaPostsWithVotes($postType: Int!, $limit: Int! = 10, $voter: String!) {
+        posts(limit: $limit, where: {type: {id: {_eq: $postType}}, onchain_link: {onchain_referendum_id: {_is_null: false}}}, order_by: {onchain_link: {onchain_referendum_id: desc}}) {
             id
-            vote
-            voter
-            referendum {
-                referendumId
+            title
+            author {
+                ...authorFields
             }
-            conviction
-            lockedValue
+            created_at
+            updated_at
+            type {
+                name
+                id
+            }
+            topic {
+                id
+                name
+            }
+            onchain_link {
+                id
+                onchain_referendum_id
+                onchain_referendum {
+                    id
+                    end
+                    referendumStatus(last: 1) {
+                        id
+                        status
+                    }
+                    referendumVote(where: {voter: $voter}) {
+                        conviction
+                        id
+                        lockedValue
+                        vote
+                        voter
+                    }
+                }
+                proposer_address
+            }
         }
     }
+    ${authorFields}
 `;
