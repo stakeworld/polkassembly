5502828a0fbaf408c88251ef15d3e6e34d3a68c2
This-Is-Prince <this.is.kumar.prince@gmail.com>
2022-10-20 22:16:59 +0530

    feat(VoteInfo): new api integrated for vote info

--- front-end/src/components/Post/GovernanceSideBar/Referenda/ReferendumVoteInfo.tsx
+++ front-end/src/components/Post/GovernanceSideBar/Referenda/ReferendumVoteInfo.tsx
@@ -18,6 +18,7 @@ import Loader from 'src/ui-components/Loader';
 import PassingInfo from 'src/ui-components/PassingInfo';
 import VoteProgress from 'src/ui-components/VoteProgress';
 import formatBnBalance from 'src/util/formatBnBalance';
+import getNetwork from 'src/util/getNetwork';
 
 interface Props {
 	className?: string
@@ -52,6 +53,16 @@ const ReferendumVoteInfo = ({ className, referendumId, threshold, setLastVote, i
 		// and devide by 100 to have percentage --> 12.00%
 		return turnout.muln(10000).div(totalIssuance).toNumber()/100;
 	} , [turnout, totalIssuance]);
+	const [voteInfo, setVoteInfo] = useState<any | null>(null);
+	const turnoutPercentage2 = useMemo( () => {
+		if (totalIssuance.isZero()) {
+			return 0;
+		}
+		// BN doens't handle floats. If we devide a number by a bigger number (12/100 --> 0.12), the result will be 0
+		// therefore, we first multiply by 10 000, which gives (120 000/100 = 1200) go to Number which supports floats
+		// and devide by 100 to have percentage --> 12.00%
+		return voteInfo?.turnout.muln(10000).div(totalIssuance).toNumber()/100;
+	} , [voteInfo?.turnout, totalIssuance]);
 
 	const { addresses } = useContext(UserDetailsContext);
 
@@ -74,6 +85,56 @@ const ReferendumVoteInfo = ({ className, referendumId, threshold, setLastVote, i
 		},
 		[ayeVotes, ayeVotesWithoutConviction, isPassing, nayVotes, nayVotesWithoutConviction, threshold, totalIssuance]
 	);
+	const getThreshold2 = useMemo(
+		() => {
+			if (!voteInfo?.vote_threshold || voteInfo?.isPassing === null) return ZERO;
+			// if the referendum is passing, we're interested in the failing threshold
+			if (voteInfo?.isPassing) {
+				const res = getFailingThreshold({
+					ayes: voteInfo?.aye_amount,
+					ayesWithoutConviction: voteInfo?.aye_without_conviction,
+					threshold: voteInfo?.vote_threshold,
+					totalIssuance
+				});
+				return res.isValid && res.failingThreshold ? res.failingThreshold : ZERO;
+			} else {
+				const res = getPassingThreshold({
+					nays: voteInfo?.nay_amount,
+					naysWithoutConviction: voteInfo?.nay_without_conviction,
+					threshold: voteInfo?.vote_threshold,
+					totalIssuance
+				});
+				return res.isValid && res.passingThreshold ? res.passingThreshold : ZERO;
+			}
+		},
+		[voteInfo?.aye_amount, voteInfo?.aye_without_conviction, voteInfo?.isPassing, voteInfo?.nay_amount, voteInfo?.nay_without_conviction, voteInfo?.vote_threshold, totalIssuance]
+	);
+	useEffect(() => {
+		// eslint-disable-next-line quotes
+		fetch(`https://${getNetwork()}.api.subscan.io/api/scan/democracy/referendum`, { body: JSON.stringify({ referendum_index: referendumId }), method: 'POST' }).then(async (res)=>{
+			try {
+				const response = await res.json();
+				const info = response?.data?.info;
+				if (info) {
+					if (info.status === 'notPassed'){
+						info.isPassing = false;
+					} else {
+						info.isPassing = true;
+					}
+					info.aye_amount = new BN(info.aye_amount);
+					info.aye_without_conviction = new BN(info.aye_without_conviction);
+					info.nay_amount = new BN(info.nay_amount);
+					info.nay_without_conviction = new BN(info.nay_without_conviction);
+					info.turnout = new BN(info.turnout);
+					setVoteInfo(info);
+				}
+			} catch (error) {
+				setVoteInfo(null);
+			}
+		}).catch(() => {
+			setVoteInfo(null);
+		});
+	},[referendumId]);
 
 	useEffect(() => {
 		if (!api) {
@@ -176,43 +237,73 @@ const ReferendumVoteInfo = ({ className, referendumId, threshold, setLastVote, i
 
 	return (
 		<>
-			{isPassingInfoShow && <PassingInfo isPassing={isPassing}/>}
-			<Card className={loadingStatus.isLoading ? `LoaderWrapper ${className}` : className}>
-				{loadingStatus.isLoading
-					? <Loader text={loadingStatus.message} timeout={30000} timeoutText='Api is unresponsive.'/>
-					: <>
-						{
-							isPassing === null
-								? <Loader className={'progressLoader'} text={'Loading vote progress'} timeout={90000} timeoutText='Vote calculation failed' delayText='The results should be available soon!' delayTextTimeout={30000}/>
-								: <VoteProgress
-									ayeVotes={ayeVotes}
-									className='vote-progress'
-									isPassing={isPassing}
-									threshold={getThreshold}
-									nayVotes={nayVotes}
-									thresholdType={threshold}
-								/>
-						}
+			{!voteInfo?<>
+				{isPassingInfoShow && <PassingInfo isPassing={isPassing}/>}
+				<Card className={loadingStatus.isLoading ? `LoaderWrapper ${className}` : className}>
+					{loadingStatus.isLoading
+						? <Loader text={loadingStatus.message} timeout={30000} timeoutText='Api is unresponsive.'/>
+						: <>
+							{
+								isPassing === null
+									? <Loader className={'progressLoader'} text={'Loading vote progress'} timeout={90000} timeoutText='Vote calculation failed' delayText='The results should be available soon!' delayTextTimeout={30000}/>
+									: <VoteProgress
+										ayeVotes={ayeVotes}
+										className='vote-progress'
+										isPassing={isPassing}
+										threshold={getThreshold}
+										nayVotes={nayVotes}
+										thresholdType={threshold}
+									/>
+							}
 
-						<Grid columns={3} divided>
-							<Grid.Row>
-								<Grid.Column>
-									<h6>Turnout {turnoutPercentage > 0 && <span className='turnoutPercentage'>({turnoutPercentage}%)</span>}</h6>
-									<div>{formatBnBalance(turnout, { numberAfterComma: 2, withUnit: true })}</div>
-								</Grid.Column>
-								<Grid.Column>
-									<h6>Aye <HelperTooltip content='Aye votes without taking conviction into account'/></h6>
-									<div>{formatBnBalance(ayeVotesWithoutConviction, { numberAfterComma: 2, withUnit: true })}</div>
-								</Grid.Column>
-								<Grid.Column>
-									<h6>Nay <HelperTooltip content='Nay votes without taking conviction into account'/></h6>
-									<div>{formatBnBalance(nayVotesWithoutConviction, { numberAfterComma: 2, withUnit: true })}</div>
-								</Grid.Column>
-							</Grid.Row>
-						</Grid>
-					</>
-				}
-			</Card>
+							<Grid columns={3} divided>
+								<Grid.Row>
+									<Grid.Column>
+										<h6>Turnout {turnoutPercentage > 0 && <span className='turnoutPercentage'>({turnoutPercentage}%)</span>}</h6>
+										<div>{formatBnBalance(turnout, { numberAfterComma: 2, withUnit: true })}</div>
+									</Grid.Column>
+									<Grid.Column>
+										<h6>Aye <HelperTooltip content='Aye votes without taking conviction into account'/></h6>
+										<div>{formatBnBalance(ayeVotesWithoutConviction, { numberAfterComma: 2, withUnit: true })}</div>
+									</Grid.Column>
+									<Grid.Column>
+										<h6>Nay <HelperTooltip content='Nay votes without taking conviction into account'/></h6>
+										<div>{formatBnBalance(nayVotesWithoutConviction, { numberAfterComma: 2, withUnit: true })}</div>
+									</Grid.Column>
+								</Grid.Row>
+							</Grid>
+						</>
+					}
+				</Card>
+			</>:<>
+				{isPassingInfoShow && <PassingInfo isPassing={voteInfo?.isPassing}/>}
+				<Card className={loadingStatus.isLoading ? `LoaderWrapper ${className}` : className}>
+					<VoteProgress
+						ayeVotes={voteInfo?.aye_amount}
+						className='vote-progress'
+						isPassing={voteInfo?.isPassing}
+						threshold={getThreshold2}
+						nayVotes={voteInfo?.nay_amount}
+						thresholdType={voteInfo?.vote_threshold}
+					/>
+					<Grid columns={3} divided>
+						<Grid.Row>
+							<Grid.Column>
+								<h6>Turnout {turnoutPercentage2 > 0 && <span className='turnoutPercentage'>({turnoutPercentage2}%)</span>}</h6>
+								<div>{formatBnBalance(voteInfo?.turnout, { numberAfterComma: 2, withUnit: true })}</div>
+							</Grid.Column>
+							<Grid.Column>
+								<h6>Aye <HelperTooltip content='Aye votes without taking conviction into account'/></h6>
+								<div>{formatBnBalance(voteInfo?.aye_without_conviction, { numberAfterComma: 2, withUnit: true })}</div>
+							</Grid.Column>
+							<Grid.Column>
+								<h6>Nay <HelperTooltip content='Nay votes without taking conviction into account'/></h6>
+								<div>{formatBnBalance(voteInfo?.nay_without_conviction, { numberAfterComma: 2, withUnit: true })}</div>
+							</Grid.Column>
+						</Grid.Row>
+					</Grid>
+				</Card>
+			</>}
 		</>
 	);
 };
