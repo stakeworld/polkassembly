5513f1a072739c79951b5430142bd8bc9f3bcd43
rajdeep7Singh <sdrajdeep96@gmail.com>
2022-11-15 18:54:19 +0530

    minor fix

--- node-watcher/src/tasks/index.ts
+++ node-watcher/src/tasks/index.ts
@@ -64,7 +64,7 @@ export const nomidotTasks: NomidotTask[] = [
   createChildBountyStatus,
   createCouncilMotionVotes,
   createReferendumVote,
+  createPreimageV2,
   createReferendumV2,
   createReferendumStatusV2,
-  createPreimageV2,
 ];
