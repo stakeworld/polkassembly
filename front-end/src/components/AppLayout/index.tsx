8ef7f58d49fc30574f7658b962b3cf16b203380d
aadarsh012 <aadarsh012@gmail.com>
2022-11-18 01:10:37 +0530

    updated sidedrawer with antd drawer

--- front-end/src/components/AppLayout/index.tsx
+++ front-end/src/components/AppLayout/index.tsx
@@ -4,7 +4,7 @@
 
 import { BellOutlined, BookOutlined, DownOutlined, LogoutOutlined, SettingOutlined, UserOutlined } from '@ant-design/icons';
 import styled from '@xstyled/styled-components';
-import { Avatar, Dropdown, Layout, Menu, MenuProps } from 'antd';
+import { Avatar, Drawer, Dropdown, Layout, Menu, MenuProps } from 'antd';
 import { ItemType } from 'antd/lib/menu/hooks/useItems';
 import React, { memo, ReactNode, useState } from 'react';
 import { Link, useLocation, useNavigate } from 'react-router-dom';
@@ -158,7 +158,7 @@ const collapsedItems: MenuProps['items'] = [
 
 const AppLayout = ({ className }: { className?:string }) => {
 	const { setUserDetailsContextState, username, picture } = useUserDetailsContext();
-	const [sidebarCollapsed, setSidebarCollapsed] = useState<boolean>(true);
+	const [sidedrawer, setSidedrawer] = useState<boolean>(false);
 	const navigate = useNavigate();
 	const { pathname } = useLocation();
 
@@ -166,11 +166,7 @@ const AppLayout = ({ className }: { className?:string }) => {
 		if(menuItem.key === 'noNavigate') return;
 
 		navigate(menuItem.key);
-		// only for mobile devices
-		if (window.innerWidth < 1024) {
-			document.body.classList.remove('overflow-hidden');
-			setSidebarCollapsed(true);
-		}
+		setSidedrawer(false);
 	};
 	const [logoutMutation] = useLogoutMutation();
 
@@ -186,16 +182,16 @@ const AppLayout = ({ className }: { className?:string }) => {
 
 	return (
 		<Layout className={className}>
-			<NavHeader sidebarCollapsed={sidebarCollapsed} setSidebarCollapsed={setSidebarCollapsed} />
-			<Layout hasSider className='relative'>
-				{!sidebarCollapsed && <div onClick={() => setSidebarCollapsed(true)} className={`${sidebarCollapsed ? 'hidden' : ''} fixed top-0 left-0 w-screen h-screen z-10 opacity-20 bg-sidebarBlue `}></div>}
+			<NavHeader sidedrawer={sidedrawer} setSidedrawer={setSidedrawer}  />
+			<Layout hasSider>
 				<Sider
 					trigger={null}
-					collapsible
-					collapsed={sidebarCollapsed}
-					onMouseOver={() => setSidebarCollapsed(false)}
-					onMouseLeave={() => setSidebarCollapsed(true)}
-					className={`${sidebarCollapsed ? 'hidden overflow-y-hidden': 'min-w-[256px]'} sidebar bg-white lg:block bottom-0 left-0 h-screen overflow-y-auto fixed z-40`}
+					collapsible={false}
+					collapsed={true}
+					onMouseOver={() => setSidedrawer(true)}
+					style={{ transform: sidedrawer ? 'translateX(-60px)' : 'translateX(0px)', transitionDuration: '0.3s' }}
+					// onMouseLeave={() => setSidebarCollapsed(true)}
+					className={'hidden overflow-y-hidden sidebar bg-white lg:block bottom-0 left-0 h-screen overflow-y-auto fixed z-40'}
 				>
 					<Menu
 						theme="light"
@@ -203,14 +199,26 @@ const AppLayout = ({ className }: { className?:string }) => {
 						selectedKeys={[pathname]}
 						defaultOpenKeys={['democracy_group', 'treasury_group', 'council_group', 'tech_comm_group']}
 						items={
-							sidebarCollapsed ? username ?
-								[getUserDropDown(handleLogout, picture, username), ...collapsedItems] : collapsedItems
-								: username ? [getUserDropDown(handleLogout, picture, username), ...items] : items
-						}
+							username?
+								[getUserDropDown(handleLogout, picture, username), ...collapsedItems]
+								: collapsedItems}
 						onClick={handleMenuClick}
 						className={`${username?'auth-sider-menu':''} mt-[60px]`}
 					/>
 				</Sider>
+				<Drawer placement='left' closable={false} onClose={() => setSidedrawer(false)} open={sidedrawer} getContainer={false} style={{ bottom:0, left:0, position: 'fixed', top: '60px' }}>
+					<Menu
+						theme="light"
+						mode="inline"
+						selectedKeys={[pathname]}
+						defaultOpenKeys={['democracy_group', 'treasury_group', 'council_group', 'tech_comm_group']}
+						items={username
+							? [getUserDropDown(handleLogout, picture, username), ...items]
+							: items}
+						onClick={handleMenuClick}
+						className={`${username?'auth-sider-menu':''} mt-[60px]`}
+					/>
+				</Drawer>
 				<Layout className='min-h-[calc(100vh - 10rem)] flex flex-row'>
 					{/* Dummy Collapsed Sidebar for auto margins */}
 					<div className="hidden lg:block bottom-0 left-0 w-[80px] -z-50"></div>
@@ -229,6 +237,20 @@ const CustomContent = memo(function CustomContent() {
 });
 
 export default styled(AppLayout)`
+
+.ant-drawer-content-wrapper{
+	max-width: 256px !important;
+	box-shadow: none !important;
+	min-width: 60px !important;
+
+}
+.ant-drawer-body{
+	padding: 0 !important;
+
+	ul{
+		margin-top: 0 !important;
+	}
+}
 
 .ant-menu-item-selected {
 	background: #fff !important;
