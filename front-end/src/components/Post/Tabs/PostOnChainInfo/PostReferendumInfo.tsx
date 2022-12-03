158c69c10af4b5385949face2902d1222401f8e7
This-Is-Prince <this.is.kumar.prince@gmail.com>
2022-12-03 17:31:31 +0530

    style: onchainInfo metadata style changed to list

--- front-end/src/components/Post/Tabs/PostOnChainInfo/PostReferendumInfo.tsx
+++ front-end/src/components/Post/Tabs/PostOnChainInfo/PostReferendumInfo.tsx
@@ -2,7 +2,6 @@
 // This software may be modified and distributed under the terms
 // of the Apache-2.0 license. See the LICENSE file for details.
 
-import { Col, Row } from 'antd';
 import * as moment from 'moment';
 import * as React from 'react';
 import BlockCountdown from 'src/components/BlockCountdown';
@@ -34,66 +33,67 @@ const PostReferendumInfo = ({ onchainLink, setOtherProposalsSidebarAddr }: Props
 	const { delay, end, referendumStatus, preimage, voteThreshold } = onchainReferendum[0];
 	const { metaDescription, method, preimageArguments } = preimage || {};
 	const { blockNumber, status } = referendumStatus?.[0] || {};
-
+	console.log(onchainReferendum[0]);
 	return (
 		<>
 			<OnchainInfoWrapper>
-				<Row>
-					<Col span={24}>
-						<h6>Proposer</h6>
-						<Address address={proposerAddress}/>
-						<div className='text-pink_primary cursor-pointer mt-3' onClick={() => setOtherProposalsSidebarAddr(proposerAddress)}>
-							View Other Proposals
-						</div>
-					</Col>
-					{method &&
-						<Col span={12}>
-							<h6>Method</h6>
-							<div className='text-navBlue'>{method}</div>
-						</Col>
-					}
-					{end &&
-						<Col span={12}>
-							{status === 'Started'
-								?
-								<>
-									<h6>End</h6>
+				<ul className='list-none flex flex-col gap-y-2'>
+					<li className='grid grid-cols-8 gap-x-5 border-b py-1.5'>
+						<h6 className='col-span-2'>Proposer</h6>
+						<article className='flex gap-x-2 col-span-6'>
+							<Address address={proposerAddress}/>
+							<div className='text-pink_primary cursor-pointer ml-auto' onClick={() => setOtherProposalsSidebarAddr(proposerAddress)}>
+								View Other Proposals
+							</div>
+						</article>
+					</li>
+					{method && <li className='grid grid-cols-8 gap-x-5 border-b py-1.5'>
+						<h6 className='col-span-2'>Method</h6>
+						<div className='text-navBlue col-span-6'>{method}</div>
+					</li>}
+					{end && <li className='grid grid-cols-8 gap-x-5 border-b py-1.5'>
+						{status === 'Started'
+							?
+							<>
+								<h6 className='col-span-2'>End</h6>
+								<div className='col-span-6'>
 									<BlockCountdown className='text-navBlue' endBlock={end}/>
-								</>
-								:
-								<>
-									<h6>Ended</h6>
-									<div className='text-navBlue'>{moment.utc(blockNumber?.startDateTime).format('DD MMM YYYY, HH:mm:ss')}</div>
-								</>
-							}
-						</Col>}
+								</div>
+							</>
+							:
+							<>
+								<h6 className='col-span-2'>Ended</h6>
+								<div className='text-navBlue col-span-6'>{moment.utc(blockNumber?.startDateTime).format('DD MMM YYYY, HH:mm:ss')}</div>
+							</>
+						}
+					</li>}
 					{(delay || delay === 0) &&
-						<Col span={12}>
-							<h6>Delay</h6>
-							<div className='text-navBlue'><BlocksToTime blocks={delay} /></div>
-						</Col>}
+						<li className="grid grid-cols-8 gap-x-5 border-b py-1.5">
+							<h6 className='col-span-2'>Delay</h6>
+							<div className='text-navBlue col-span-6'><BlocksToTime blocks={delay} /></div>
+						</li>
+					}
 					{voteThreshold &&
-						<Col span={12}>
-							<h6>Vote threshold</h6>
-							<div className='text-navBlue'>{voteThreshold}</div>
-						</Col>}
-					{method &&
-					<>
-						<div className='arguments max-w-full'>
-							{preimageArguments && preimageArguments.length
-								? <ArgumentsTableJSONView postArguments={preimageArguments} showAccountArguments={true}  />
-								: null}
-						</div>
-					</>}
-					{metaDescription &&
-					<Col span={24}>
-						<h6>Description</h6>
-						<p className='text-navBlue leading-6'>{metaDescription}</p>
-					</Col>}
-					<Col span={24}>
-						<ExternalLinks isReferendum={true} onchainId={onchainLink.onchain_referendum_id} />
-					</Col>
-				</Row>
+						<li className="grid grid-cols-8 gap-x-5 border-b py-1.5">
+							<h6 className='col-span-2'>Vote threshold</h6>
+							<div className='text-navBlue col-span-6'>{voteThreshold}</div>
+						</li>
+					}
+				</ul>
+				{method &&
+				<>
+					<div className='arguments max-w-full mt-5'>
+						{preimageArguments && preimageArguments.length
+							? <ArgumentsTableJSONView postArguments={preimageArguments} showAccountArguments={true}  />
+							: null}
+					</div>
+				</>}
+				{metaDescription &&
+				<div className='grid grid-cols-6 gap-x-2 mt-2'>
+					<h6 className='col-span-1'>Description</h6>
+					<p className='text-navBlue leading-6 col-span-5'>{metaDescription}</p>
+				</div>}
+				<ExternalLinks className='mt-5' isReferendum={true} onchainId={onchainLink.onchain_referendum_id} />
 			</OnchainInfoWrapper>
 		</>
 	);
