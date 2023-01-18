74baddba3c1ad97ae26e7c87085216a13c24ff71
Nikhil Ranjan <niklabh811@gmail.com>
2023-01-18 18:32:45 +0100

    subscan api key

--- front-end/src/global/apiKeys.ts
+++ front-end/src/global/apiKeys.ts
@@ -2,5 +2,5 @@
 // This software may be modified and distributed under the terms
 // of the Apache-2.0 license. See the LICENSE file for details.
 
-export const REACT_APP_SUBSCAN_API_KEY = '056b677410ac226bea971a3e03de66fa';
+export const REACT_APP_SUBSCAN_API_KEY = '74d1845ab15f4b889a64dfef074ef222';
 export const FIREBASE_API_KEY = process.env.FIREBASE_API_KEY;
