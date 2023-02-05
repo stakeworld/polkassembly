d97baf3fe458bde07fd2bb698a110615d76130fd
alphainfinitus <kumar.mridul000@gmail.com>
2023-02-05 17:51:28 +0530

    split and empty state fix for referendum v2

--- front-end/src/components/Post/GovernanceSideBar/Referenda/ReferendumV2VoteInfo.tsx
+++ front-end/src/components/Post/GovernanceSideBar/Referenda/ReferendumV2VoteInfo.tsx
@@ -2,7 +2,7 @@
 // This software may be modified and distributed under the terms
 // of the Apache-2.0 license. See the LICENSE file for details.
 
-import { DislikeFilled, LeftOutlined, LikeFilled, MinusCircleFilled, RightOutlined } from '@ant-design/icons';
+import { DislikeFilled, LeftOutlined, LikeFilled, MinusCircleFilled, RightOutlined, StopFilled } from '@ant-design/icons';
 import { Segmented } from 'antd';
 import React, { useCallback, useEffect, useState } from 'react';
 import { subsquidApiHeaders } from 'src/global/apiHeaders';
@@ -97,43 +97,44 @@ const ReferendumV2VoteInfo = ({ className, referendumId } : Props) => {
 
 		if(loading) return <GovSidebarCard className={className}><LoadingState /></GovSidebarCard>;
 
-		if(votesList.length > 0) {
-			return (
-				<GovSidebarCard className={className}>
-					<div className="flex justify-between mb-6 bg-white z-10">
-						<h6 className='dashboard-heading'>Voters</h6>
-					</div>
+		return (
+			<GovSidebarCard className={className}>
+				<div className="flex justify-between mb-6 bg-white z-10">
+					<h6 className='dashboard-heading'>Voters</h6>
+				</div>
 
-					<div className="w-full flex items-center justify-center mb-8">
-						<Segmented
-							block
-							className='px-3 py-2 rounded-md w-full'
-							size="large"
-							defaultValue={fetchDecision}
-							onChange={(value) => {
-								setFetchDecision(String(value) as DecisionType);
-								setOffset(0);
-							}}
-							options={[
-								{
-									icon: <LikeFilled />,
-									label: 'Ayes',
-									value: 'yes'
-								},
-								{
-									icon: <DislikeFilled />,
-									label: 'Nays',
-									value: 'no'
-								},
-								{
-									icon: <MinusCircleFilled />,
-									label: 'Abstain',
-									value: 'abstain'
-								}
-							]}
-						/>
-					</div>
+				<div className="w-full flex items-center justify-center mb-8">
+					<Segmented
+						block
+						className='px-3 py-2 rounded-md w-full'
+						size="large"
+						defaultValue={fetchDecision}
+						onChange={(value) => {
+							setFetchDecision(String(value) as DecisionType);
+							setOffset(0);
+						}}
+						options={[
+							{
+								label: <div className='flex items-center justify-center'><LikeFilled className='mr-1.5' /> <span>Ayes</span></div>,
+								value: 'yes'
+							},
+							{
+								label: <div className='flex items-center justify-center'><DislikeFilled className='mr-1.5' /> <span>Nays</span></div>,
+								value: 'no'
+							},
+							{
+								label: <div className='flex items-center justify-center'><MinusCircleFilled className='mr-1.5' /> <span>Abstain</span></div>,
+								value: 'abstain'
+							},
+							{
+								label: <div className='flex items-center justify-center'><StopFilled className='mr-1.5' /> <span>Split</span></div>,
+								value: 'split'
+							}
+						]}
+					/>
+				</div>
 
+				{votesList.length > 0 ?
 					<div className='flex flex-col text-xs xl:text-sm xl:max-h-screen gap-y-1 overflow-y-auto px-0 text-sidebarBlue'>
 						<div className='flex text-xs items-center justify-between mb-9 font-semibold'>
 							<div className='w-[110px]'>Voter</div>
@@ -170,22 +171,16 @@ const ReferendumV2VoteInfo = ({ className, referendumId } : Props) => {
 						)}
 
 					</div>
+					: <PostEmptyState />
+				}
 
-					<div className="flex items-center justify-center pt-6 bg-white z-10">
-						<div className={`mr-5 flex items-center ${offset === 0 ? ' cursor-default' : 'cursor-pointer hover:text-pink_primary'}`} onClick={() => handlePagination('prev')}><LeftOutlined className='mr-1' /> Prev</div>
-						<div className={`ml-5  flex items-center ${votesList.length < 10 ? ' cursor-default' : 'cursor-pointer hover:text-pink_primary'}`} onClick={() => handlePagination('next')}>Next <RightOutlined  className='ml-1' /></div>
-					</div>
+				<div className="flex items-center justify-center pt-6 bg-white z-10">
+					<div className={`mr-5 flex items-center ${offset === 0 ? ' cursor-default' : 'cursor-pointer hover:text-pink_primary'}`} onClick={() => handlePagination('prev')}><LeftOutlined className='mr-1' /> Prev</div>
+					<div className={`ml-5  flex items-center ${votesList.length < 10 ? ' cursor-default' : 'cursor-pointer hover:text-pink_primary'}`} onClick={() => handlePagination('next')}>Next <RightOutlined  className='ml-1' /></div>
+				</div>
 
-				</GovSidebarCard>
-			);
-		}
-
-		if(votesList.length === 0 && offset > 0) {
-			<div className="flex items-center justify-center pt-6 bg-white z-10">
-				<div className={`mr-5 flex items-center ${offset === 0 ? ' cursor-default' : 'cursor-pointer hover:text-pink_primary'}`} onClick={() => handlePagination('prev')}><LeftOutlined className='mr-1' /> Prev</div>
-				<div className={`ml-5  flex items-center ${votesList.length < 10 ? ' cursor-default' : 'cursor-pointer hover:text-pink_primary'}`} onClick={() => handlePagination('next')}>Next <RightOutlined  className='ml-1' /></div>
-			</div>;
-		}
+			</GovSidebarCard>
+		);
 
 		return <GovSidebarCard className={className}>
 			<div className="flex justify-between mb-6 bg-white z-10">
