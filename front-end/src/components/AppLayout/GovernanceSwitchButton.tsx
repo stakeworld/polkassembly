0a3ff38bdf66b0765a9d978758cbe069fd6c2800
Nikhil Ranjan <niklabh811@gmail.com>
2023-03-06 16:03:54 +0100

    Fix governance name

--- front-end/src/components/AppLayout/GovernanceSwitchButton.tsx
+++ front-end/src/components/AppLayout/GovernanceSwitchButton.tsx
@@ -19,7 +19,7 @@ const GovernanceSwitchButton = ({ className } : Props) => {
 		<div className={`${className} v2-button-wrapper`}>
 			<Link className='v2-box font-semibold rounded-md text-sidebarBlue hover:text-pink_primary flex items-center' to={isGov2Route ? '/' : '/gov-2'}>
 				<p className='mr-2 -mt-1'><SyncOutlined /></p>
-				<p> {isGov2Route ? 'Governance V1' : 'Governance V2'} </p>
+				<p> {isGov2Route ? 'Governance V1' : 'OpenGov'} </p>
 			</Link>
 		</div>
 	);
