674c23c70db6aadc2fb46dab31a1bad85d9d8d11
Nikhil Ranjan <niklabh811@gmail.com>
2023-03-22 11:52:16 +0100

    lint

--- auth-server/src/index.ts
+++ auth-server/src/index.ts
@@ -66,11 +66,11 @@ app.use(cors());
 app.set('host', '0.0.0.0');
 app.set('port', process.env.PORT || 8080);
 app.set('json spaces', 2); // number of spaces for indentation
-var rawBodySaver = function (req: any, res: any, buf: any, encoding: any) {
-  if (buf && buf.length) {
-    console.log(buf.toString(encoding || 'utf8'));
-  }
-}
+const rawBodySaver = function (req: any, res: any, buf: any, encoding: any) {
+	if (buf && buf.length) {
+		console.log(buf.toString(encoding || 'utf8'));
+	}
+};
 app.use(bodyParser.json({ verify: rawBodySaver }));
 app.use(cookieParser());
 app.use(expressValidator());
